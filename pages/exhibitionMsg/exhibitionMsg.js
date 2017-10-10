66239455ad4bd38fec463e28b667ee55a65c8f18
957685825 <957685825@qq.com>
2017-10-10 15:23:44 +0800

    s

--- pages/exhibitionMsg/exhibitionMsg.js
+++ pages/exhibitionMsg/exhibitionMsg.js
@@ -1,4 +1,4 @@
-// pages/exhibition.js
+// pages/exhibitionMsg/exhibitionMsg.js
 Page({
 
   /**
@@ -7,18 +7,14 @@ Page({
   data: {
   
   },
-  toast:function(){
-    wx.navigateTo({
-      url: '../exhibitionMsg/exhibitionMsg'
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
   
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
